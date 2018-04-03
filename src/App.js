19459cfa81633b15e2db30e6c97bdf1110750028
Calvin Kwong <ckwong@us.costar.local>
2018-04-03 10:51:33 -0700

    add test component to ensure everything is linked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Test from './Components/test';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Test </h1>
         <p>testing</p>
-        <p>asdf</p>
+        <Test />
       </div>
     );
   }
