d2e3d624c0b02985d85c3d1ef27e17f10ee6de0a
Calvin Kwong <ckwong@us.costar.local>
2018-04-04 14:31:01 -0700

    update listing json data, work on itemDetail component

--- src/Components/Containers/itemDetail.js
+++ src/Components/Containers/itemDetail.js
@@ -18,6 +18,11 @@ class ItemDetail extends Component{
     }
 
     renderItem(){
+        // converts api integer into dollar value
+        function convertNum(number){
+            return number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
+        }
+
         let data = this.props.item[0];
         if(!data){
             return(
@@ -27,30 +32,41 @@ class ItemDetail extends Component{
         return(
         data.map(function(info){
             return(
-                <div key={info.franchse_id}>
-                    <p>{info.franchise_id}</p>
-                    <p>{info.name}</p>
-                    <p>{info.short_name}</p>
-                    <p>{info.address}</p>
-                    <p>{info.address_2}</p>
-                    <p>{info.city}</p>
-                    <p>{info.state_id}</p>
-                    <p>{info.zip}</p>
-                    <p>{info.country_id}</p>
-                    <p>{info.local_phone}</p>
-                    <p>{info.min_capital_min}</p>
-                    <p>{info.min_capital_max}</p>
-                    <p>{info.min_worth_min}</p>
-                    <p>{info.min_worth_max}</p>
-                    <p>{info.franchise_fee_min}</p>
-                    <p>{info.franchise_fee_max}</p>
-                    <p>{info.total_investment_min}</p>
-                    <p>{info.total_investment_max}</p>
-                    <p>{info.low_cost}</p>
-                    <p>{info.business_opportunity}</p>
-                    <p>{info.high_capital}</p>
-                    <p>{info.short_description}</p>                    
-                    <p>{info.display_image}</p>
+                <div>
+                    <div>
+                        <h1>{info.name}</h1>
+                        <p>{info.shortDescription}</p>
+                        <p>{info.address}, {info.city}, {info.stateId}, {info.zip}</p>
+                        <p>Minimum Liquid Capital: <b>${convertNum(info.minCapitalMin)}</b></p>
+                        <p>Total Investment Range <b>${convertNum(info.totalInvestmentMin)}</b> - <b>${convertNum(info.totalInvestmentMax)}</b></p>
+                    </div>
+                    <div key={info.franchseId}>
+                        <h3>Sample Data</h3>
+                        <p>franchiseId: {info.franchiseId}</p>
+                        <p>name: {info.name}</p>
+                        <p>address: {info.address}</p>
+                        <p>address2: {info.address2}</p>
+                        <p>city: {info.city}</p>
+                        <p>stateId: {info.stateId}</p>
+                        <p>zip: {info.zip}</p>
+                        <p>countryId: {info.countryId}</p>
+                        <p>localPhone: {info.localPhone}</p>
+                        <p>minCapitalMin: {info.minCapitalMin}</p>
+                        <p>minCapitalMax: {info.minCapitalMax}</p>
+                        <p>minWorthMin: {info.minWorthMin}</p>
+                        <p>minWorthMax: {info.minWorthMax}</p>
+                        <p>franchiseFeeMin: {info.franchiseFeeMin}</p>
+                        <p>franchiseFeeMax: {info.franchiseFeeMax}</p>
+                        <p>totalInvestmentMin: {info.totalInvestmentMin}</p>
+                        <p>totalInvestmentMax: {info.totalInvestmentMax}</p>
+                        <p>lowCost: {info.lowCost}</p>
+                        <p>businessOpportunity: {info.businessOpportunity}</p>
+                        <p>highCapital: {info.highCapital}</p>
+                        <p>shortDescription: {info.shortDescription}</p>
+                        <p>generalDescription: {info.generalDescription}</p>
+                        <p>createdDate: {info.createdDate}</p>                    
+                        <p>display_image: {info.display_image}</p>
+                    </div>
                 </div>
             )
         })
